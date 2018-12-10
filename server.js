0aa93f2badc23dfe0ad62c897396e0c3b65e9b08
YJK <kaky0910@gmail.com>
2018-12-10 16:13:41 +0900

    oracle

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const challenge = require('./database/dailystatic');
+const moment = require('moment');
 
 var app = express();
 
 app.get('/api/statics',(req,res) => {
     challenge.addDailyStatics();
+    if(moment().subtract('days',1).format('DD') === moment().subtract('days',1).daysInMonth()){
+        ///habit 추가
+    }
 });
 
 app.listen('3000');
